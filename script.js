2d6bce91a38786477076a62ea07f3bf2cb8afb77
Ben Christensen <ben.eric.christensen@gmail.com>
2022-01-05 09:00:23 -0700

    more or less done. might have to add a drop down menu with selectors per the acceptance criteria, but ui wise that seems like a nightmare

--- script.js
+++ script.js
@@ -8,6 +8,20 @@ const lowercaseEl = document.getElementById("lowercase");
 const symbolsEl = document.getElementById("symbols");
 const numbersEl = document.getElementById("numbers");
 
+//jquery function that generates the numbered options for password length
+$(function(){
+    var $select = $("#length");
+    for (i=8;i<=128;i++){
+        $select.append($('<option></option>').val(i).html(i))
+    }
+});
+
+//arrays for different character types
+const upperArr = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
+const lowerArr = "abcdefghijklmnopqrstuvwxyz";
+const numbersArr = "0123456789";
+const symbolsArr = '!@#$%^&*(){}[]=<>/;.';
+
 
 // Write password to the #password input
 function writePassword() {
@@ -17,63 +31,42 @@ function writePassword() {
     const hasSymbols = symbolsEl.checked;
     const hasNumbers = numbersEl.checked;
     let typesUsed = [];
-    let forArray = [];
+    let passwordArr = [];
     
     
     function generatePassword() {
         
         if (hasUpper === true) {
-             typesUsed.push(generateRandomUpper());
+             typesUsed += upperArr.slice(0, upperArr.length);
         }
 
-        if (hasLower === true) {
-             typesUsed.push(generateRandomLower());
-        }
+         if (hasLower === true) {
+              typesUsed += lowerArr.slice(0, lowerArr.length);
+         }
 
-        if (hasSymbols === true) {
-             typesUsed.push(generateRandomSymbol());
-        }
+         if (hasNumbers === true) {
+              typesUsed += numbersArr.slice(0, numbersArr.length);
+         }
 
-        if (hasNumbers === true) {
-             typesUsed.push(generateRandomNumber());
-        }
+          if (hasSymbols === true) {
+               typesUsed += symbolsArr.slice(0, symbolsArr.length);
+          }
 
         for (let i = 0; i < length; i++) {
             let num = Math.floor(Math.random() * typesUsed.length)
-            forArray.push(typesUsed[num]);            
+            passwordArr.push(typesUsed[num]);            
         }
     }
 
-    const password = generatePassword() 
     
-
+    generatePassword() 
+    const password = passwordArr.join("");
 
     var passwordText = document.querySelector("#password");
 
     passwordText.value = password;
-    console.log(forArray.join(''))
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-
-//String.fromCharCode is accessing from here --- https://net-comber.com/charset.html
-
-function generateRandomUpper() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function generateRandomLower() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function generateRandomNumber() {
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function generateRandomSymbol() {
-    const symbols = '!@#$%^&*(){}[]=<>/;.';
-    return symbols[Math.floor(Math.random() * symbols.length)];
-}
